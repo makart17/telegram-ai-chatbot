fc2c676c25bc23ed3ae4f64caa193b0bb3f4e9d4
makart17 <artemiymakarkin@gmail.com>
2025-05-13 22:46:42 +0500

    Fix contextLoads by mocking CommandLineRunner

--- src/test/java/com/telegramai/chatbot/ChatbotApplicationTests.java
+++ src/test/java/com/telegramai/chatbot/ChatbotApplicationTests.java
@@ -1,13 +1,20 @@
 package com.telegramai.chatbot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class ChatbotApplicationTests {
 
+	// <-- Добавляем здесь
+	@MockBean
+	private CommandLineRunner commandLineRunner;
+	// -->
+
 	@Test
 	void contextLoads() {
+		// проверяем, что контекст стартует без ошибок
 	}
-
 }
